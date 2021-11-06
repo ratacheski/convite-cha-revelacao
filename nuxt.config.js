b9ef14837a567a6eef389ac29319832040f76b1d
Rafael Ratacheski <rafael.raulino@maximatech.com.br>
2021-11-06 20:49:54 -0300

    alteração do workflow de ci

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,26 +9,22 @@ export default {
   head: {
     title: 'Convite Chá Revelação',
     htmlAttrs: {
-      lang: 'pt-BR'
+      lang: 'pt-BR',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -42,10 +38,12 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
+
+  router: {
+    base: '/convite-cha-revelacao/',
+  },
 }
