02d86b4a35dcf7aa89764c9d53c0a7260fb299c5
Rafael Ratacheski <rafael.raulino@maximatech.com.br>
2021-11-06 21:45:56 -0300

    criação de links e ajuste de texto

--- pages/_convidado.vue
+++ pages/_convidado.vue
@@ -12,15 +12,15 @@
           Baby Ratacheski vem aí!
         </span>
         <span class="col-span-10 mt-3 col-start-2 text-pink-100">
-          Para celebrar este dia, contamos com sua presença no nosso chá
+          Para celebrar este dia contamos com sua presença no nosso chá
           revelação.
         </span>
         <span class="col-span-10 col-start-2 text-pink-100">
           Será Alice ou Miguel?
         </span>
         <span class="col-span-10 col-start-2 text-pink-100">
-          Para que possamos lhe receber com as pompas que merece, pedimos que
-          confirme sua presença até o dia 26/11 no botão de confirmação abaixo.
+          Para serem recebidos com as pompas que merecem confirme sua presença
+          até o dia 26/11 no botão abaixo.
         </span>
         <img
           class="col-span-12 mt-3 lg:col-span-4 lg:col-start-5"
@@ -28,11 +28,11 @@
           alt="Data do chá"
         />
         <span class="col-span-10 col-start-2 text-pink-100">
-          Recadinho importante: Com muito carinho, disponibilizaremos um link
-          para acompanhar a revelação, caso infelizmente você esteja com algum
-          sintoma gripal, não tiver se vacinado, ou algum outro problema que lhe
-          impossibilite de estar com a gente fisicamente, poderá acompanhar
-          online a revelação do sexo do nosso bebê.
+          Recadinho importante: Disponibilizaremos um link para que você possa
+          acompanhar a revelação caso esteja com algum sintoma gripal, não tiver
+          se vacinado, ou algum outro problema que lhe impossibilite de estar
+          com a gente fisicamente. Assim você poderá participar online desse
+          momento especial.
         </span>
 
         <span class="col-span-10 col-start-2 text-pink-100 mt-3">
@@ -42,22 +42,34 @@
       <div class="flex py-4 justify-center gap-4">
         <div class="flex flex-col items-center">
           <a
-            href="https://api.whatsapp.com/send?phone=5534999251971"
+            href="https://api.whatsapp.com/send?phone=5534999251972"
             target="_blank"
           >
-            <img class="links" src="/convite-cha-revelacao/confirmacao.png" alt="Confirmação" />
+            <img
+              class="links"
+              src="/convite-cha-revelacao/confirmacao.png"
+              alt="Confirmação"
+            />
           </a>
           <span class="text-purple-200">Confirme Sua Presença</span>
         </div>
         <div class="flex flex-col items-center">
           <a href="https://meet.google.com/anw-swie-hmy" target="_blank">
-            <img class="links" src="/convite-cha-revelacao/live.png" alt="Link da live" />
+            <img
+              class="links"
+              src="/convite-cha-revelacao/live.png"
+              alt="Link da live"
+            />
           </a>
           <span class="text-pink-100">05/12 - 12H</span>
         </div>
         <div class="flex flex-col items-center">
           <a href="https://goo.gl/maps/E6g8WNstKo1SU9fW6" target="_blank">
-            <img class="links" src="/convite-cha-revelacao/localizacao.png" alt="Localização" />
+            <img
+              class="links"
+              src="/convite-cha-revelacao/localizacao.png"
+              alt="Localização"
+            />
           </a>
           <span class="text-green-100">R. Alferes Euzébio 266</span>
         </div>
